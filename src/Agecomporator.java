cb3d3a0a51f26932e66bf69a6e5c3cfb5e85b544
Luchok45 <125346721+Luchok45@users.noreply.github.com>
2023-04-24 22:28:17 +0300

    lambda commit#2

--- src/Agecomporator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class Agecomporator implements Comparator<Person>{
-        @Override
-        public int compare(Person person, Person t1) {
-            return Integer.compare(person.getAge(), t1.getAge());
-        }
-}
--- src/Namecomporator.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-public class Namecomporator implements Comparator<Person> {
-    @Override
-    public int compare(Person person, Person t1) {
-        return Integer.compare(person.getName().length(), t1.getName().length());
-    }
-}
